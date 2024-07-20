cd15802a734cfbd379265dede883993e1410920f
nader <nadermakram98@gmail.com>
2024-07-20 15:55:16 +0300

    add config for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://nadermakram.github.io',
+  base: '/astro-blog-2',
   integrations: [mdx(), sitemap(), tailwind()]
 });
